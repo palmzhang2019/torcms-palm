ba7af268c2805e850642054baa49cb59dd91b0f2
Palm <palm@gmail.com>
2022-07-16 19:11:42 +0900

    首页和列表页ok

--- static/js/script.js
+++ static/js/script.js
@@ -37,41 +37,13 @@ Amplitude.init({
         }
     },
     "songs": [
-		{
-			"name": "First Snow",
-			"artist": "Emancipator",
-			"album": "Soon It Will Be Cold Enough",
-			"url": "https://521dimensions.com/song/FirstSnow-Emancipator.mp3",
-			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
-		},
-		{
-			"name": "Intro / Sweet Glory",
-			"artist": "Jimkata",
-			"album": "Die Digital",
-			"url": "https://521dimensions.com/song/IntroSweetGlory-Jimkata.mp3",
-			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/die-digital.jpg"
-		},
-		{
-			"name": "Offcut #6",
-			"artist": "Little People",
-			"album": "We Are But Hunks of Wood Remixes",
-			"url": "https://521dimensions.com/song/Offcut6-LittlePeople.mp3",
-			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-but-hunks-of-wood.jpg"
-		},
-		{
-			"name": "Dusk To Dawn",
-			"artist": "Emancipator",
-			"album": "Dusk To Dawn",
-			"url": "https://521dimensions.com/song/DuskToDawn-Emancipator.mp3",
-			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/from-dusk-to-dawn.jpg"
-		},
-		{
-			"name": "Anthem",
-			"artist": "Emancipator",
-			"album": "Soon It Will Be Cold Enough",
-			"url": "https://521dimensions.com/song/Anthem-Emancipator.mp3",
-			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
-		}
+//		{
+//			"name": "First Snow",
+//			"artist": "Emancipator",
+//			"album": "Soon It Will Be Cold Enough",
+//			"url": "https://521dimensions.com/song/FirstSnow-Emancipator.mp3",
+//			"cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
+//		},
     ]
 });
 
